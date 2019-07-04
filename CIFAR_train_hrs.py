3a5673747385ad8fa69b9358ec6ec9fe0786a348
KieranXWang <kxw@bu.edu>
2019-07-04 11:09:57 -0400

    update hrs training

--- CIFAR_train_hrs.py
+++ CIFAR_train_hrs.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import tensorflow as tf
 import keras
+import keras.backend as K
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, Flatten, InputLayer, Reshape, Conv2D, MaxPooling2D
@@ -11,13 +12,18 @@ from project_utils import get_data, get_dimensions
 from keras_utils import construct_model_by_blocks
 
 # CIFAR data and information
-[X_train, X_test, Y_train, Y_test] = get_data(dataset='CIFAR', scale1=True, one_hot=True)
+[X_train, X_test, Y_train, Y_test] = get_data(dataset='CIFAR', scale1=True, one_hot=True, percentage=0.05)
 img_rows, img_cols, img_channels = get_dimensions('CIFAR')
 
 
 # FLAGS
+MODEL_INDICATOR = 'test_hrs[5][5]'
 STRUCTURE = [5, 5]
 nb_block = len(STRUCTURE)
+TRAINING_EPOCH = [2, 2]
+try:
+    os.makedirs('./Model/CIFAR_models/' + MODEL_INDICATOR + '/')
+except: pass
 
 # block definitions
 def block_0():
@@ -49,6 +55,12 @@ def block_1():
 
 block_definitions = [block_0, block_1]
 
+# loss definition
+def fn(correct, predicted):
+    return tf.nn.softmax_cross_entropy_with_logits(labels=correct, logits=predicted)
+# optimizer
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
 # start the training process
 trained_blocks = None
 
@@ -61,12 +73,37 @@ for block_idx in range(nb_block):
         block_channels.append(channel)
     HRS_channels.append(block_channels)
 
-for train_block_idx in range(nb_block):
-    for channel_idx in range(STRUCTURE[train_block_idx]):
+for block_idx in range(nb_block):
+    for channel_idx in range(STRUCTURE[block_idx]):
         if trained_blocks is None:
             Input = InputLayer(input_shape=(img_rows, img_cols, img_channels))
-            model_structure_list = [Input] + [channels[channel_idx] for channels in HRS_channels]
+            model_structure_list = [Input] + [block[channel_idx] for block in HRS_channels]
             model = construct_model_by_blocks(model_structure_list)
+
+            # training
+            model.compile(loss=fn, optimizer=sgd, metrics=['accuracy'])
+            model.fit(X_train, Y_train, batch_size=128, validation_data=(X_test, Y_test),
+                      nb_epoch=TRAINING_EPOCH[block_idx], shuffle=True)
+
+            # save weights of this channel
+            HRS_channels[block_idx][channel_idx].save_weights('%s_%d_%d' % (MODEL_INDICATOR, block_idx, channel_idx))
+
+            # freeze weights of this channel
+            for layer in HRS_channels[block_idx][channel_idx].layers:
+                layer.trainable = False
+
+            # reset weights of other channels
+            session = K.get_session()
+            for channel in [HRS_channels[i][channel_idx] for i in range(1, nb_block)]:
+                for layer in channel.layers:
+                    if hasattr(layer, 'kernel_initializer'):
+                        layer.kernel.initializer.run(session=session)
+                    if hasattr(layer, 'bias_initializer'):
+                        layer.bias.initializer.run(session=session)
+
+        else:
+            # model_structure_list = [trained_blocks] + [block[channel_idx] for ]
+
 
             print('debug')
 
